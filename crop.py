da145e06948c2840b641aebb3a098ed30950483f
綾波あやなみ <2574703489@qq.com>
2026-04-15 17:30:00 +0800

    add draw.py

--- crop.py
+++ crop.py
@@ -1,6 +1,6 @@
 import cv2 
 
-img=cv2.imread("graph\\opencv_logo.jpg")
+img=cv2.imread("graph/opencv_logo.jpg")
 
 crop=img[10:170,40:200]
 
---
+++ draw.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as c
+
+img=np.zeros([300,300,3],dtype=np.uint8)
+ig = np.random.randint(0, 256, size=(800, 800, 3), dtype=np.uint8)
+
+c.line(img,(100,200),(250,250),(250,0,0),2)
+
+c.rectangle(img,(30,100),(60,150),(0,250,0),7)
+c.circle(img,(150,100),20,(0,0,250),3)
+c.putText(img,'hello',(100,50),0,1,(200,200,200),2,1)
+c.imshow("img",img)
+c.waitKey()
