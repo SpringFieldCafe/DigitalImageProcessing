df5591aae2c7128e6b66c2dd352138f342e326de
綾波あやなみ <2574703489@qq.com>
2026-04-23 15:37:57 +0800

    change functions

--- Second Tutorial/code/JoinImage.py
+++ Second Tutorial/code/JoinImage.py
@@ -3,14 +3,14 @@ import numpy as np
 
 def stackImages(scale,imgArray):
     rows=len(imgArray)
-    cols=max(len(imgArray[0]),len(imgArray[1]))
+    cols= max(len(row) for row in imgArray)
     rowsAvailable=isinstance(imgArray[0],list)
     width=imgArray[0][0].shape[1]
     height=imgArray[0][0].shape[0]
     if rowsAvailable:
         for x in range(0,rows):
             for y in range(0,cols):
-                if imgArray[x][y].shape[:2]==imgArray[0][0].shape [:2]:
+                if imgArray[x][y].shape[:2]==imgArray[0][0].shape[:2]:
                     imgArray[x][y]=cv2.resize(imgArray[x][y],(0,0),None,scale,scale)
                 else:
                     imgArray[x][y]=cv2.resize(imgArray[x][y],(imgArray[0][0].shape[1],imgArray[0][0].shape[0]),None,scale,scale)
@@ -18,7 +18,6 @@ def stackImages(scale,imgArray):
                     imgArray[x][y]=cv2.cvtColor(imgArray[x][y],cv2.COLOR_GRAY2BGR)
         imgBlack=np.zeros((height,width,3),np.uint8)
         hor=[imgBlack]*rows
-        hor_con=[imgBlack]*rows
         for x in range(0,rows):
             hor[x]=np.hstack(imgArray[x])
         ver=np.vstack(hor)
