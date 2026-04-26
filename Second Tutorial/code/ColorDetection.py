7db7ca1f74cdd60f6565db4bbc4921155a1ca71d
綾波あやなみ <2574703489@qq.com>
2026-04-26 16:15:01 +0800

    add mask on ColorDetectiom.py

--- Second Tutorial/code/ColorDetection.py
+++ Second Tutorial/code/ColorDetection.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def empty(x):
+    pass
 
 def draw_3d(X, Y, Z, title, zlabel):
     fig = plt.figure()
@@ -18,21 +20,38 @@ def draw_3d(X, Y, Z, title, zlabel):
     ax.invert_yaxis()
 
 path = "Second Tutorial\\resources\\lambo.PNG"
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",0,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",0,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",0,255,empty)
 
-img = cv2.imread(path)
+while 1:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min=cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max=cv2.getTrackbarPos("Hue Max","TrackBars")
+    s_min=cv2.getTrackbarPos("Sat Min","TrackBars")
+    s_max=cv2.getTrackbarPos("Sat Max","TrackBars")
+    v_min=cv2.getTrackbarPos("Val Min","TrackBars")
+    v_max=cv2.getTrackbarPos("Val Max","TrackBars")
+    # print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower=np.array([h_min,s_min,v_min])
+    upper=np.array([h_max,s_max,v_max])
+    mask=cv2.inRange(imgHSV,lower,upper)
 
-if img is None:
-    print("图片读取失败，请检查路径")
-    exit()
+    cv2.imshow("original", img)
+    cv2.imshow("HSV", imgHSV)
+    cv2.imshow("mask",mask)
+    cv2.waitKey(1)
 
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
 
 print(imgHSV)
 print(imgHSV.shape)
-
-cv2.imshow("original", img)
-cv2.imshow("HSV", imgHSV)
-cv2.waitKey(1)
 
 # 分离 H、S、V 三个通道
 H = imgHSV[:, :, 0]
@@ -62,7 +81,7 @@ draw_3d(X_sample, Y_sample, S_sample, "3D Visualization of S Channel", "S")
 draw_3d(X_sample, Y_sample, V_sample, "3D Visualization of V Channel", "V")
 
 plt.show()
-cv2.waitKey(1)
+cv2.waitKey()
 
 
 cv2.destroyAllWindows()
