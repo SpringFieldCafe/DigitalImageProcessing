0cc62d875c7438c6a7f2eaae62d13808af141caa
綾波あやなみ <2574703489@qq.com>
2026-04-28 09:31:30 +0800

    finally finish ShapeDetectopm.py

--- Second Tutorial/code/ShapeDetection.py
+++ Second Tutorial/code/ShapeDetection.py
@@ -19,9 +19,8 @@ def getContours(img):
         area=cv2.contourArea(cnt)
         print(area)
         if area>500:
-            cv2.drawContours(imgContour,cnt,-1,(180,90,30),10)
+            vb=cv2.drawContours(imgContour,[cnt],-1,(180,90,30),10)
             peri=cv2.arcLength(cnt,True)
-            # print(peri)
             approx=cv2.approxPolyDP(cnt,0.02*peri,True)
             print(approx)
             objCor=len(approx)
@@ -39,12 +38,11 @@ def getContours(img):
                 objectType='cir'
             else:
                 objectType="None"
-
             cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),4)
             cv2.putText(imgContour,objectType
                         ,(x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.5,
                         (0,0,0),2)
-
+    return vb
 
 path='Second Tutorial\\resources\\shapes.png'
 img=cv2.imread(path)
@@ -54,8 +52,8 @@ imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur=cv2.GaussianBlur(imgGray,(7,7),1)
 imgCanny=cv2.Canny(imgBlur,50,50)
 imgBlank=np.zeros_like(img)
-getContours(imgCanny)
+v=getContours(imgCanny)
 
-imgStack=s(0.7,[[imgGray,img,imgContour],[imgCanny,imgBlur]])
+imgStack=s(0.4,[[imgGray,img,imgContour],[imgCanny,imgBlur,v]])
 cv2.imshow('st',imgStack)
 cv2.waitKey(0)
